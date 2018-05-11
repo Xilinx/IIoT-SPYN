f84014ece4569abf0ca915975d1092ab2b483df8
Kiran Vishal Thanjavur Bhaaskar <vishal94@bu.edu>
2018-05-11 14:42:37 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ import os
 import site
 import warnings
 from datetime import datetime
-
+dst_base_dir ='/opt/python3.6/lib/python3.6/site-packages/spyn'
 # Board specific package delivery setup
 def exclude_from_files(exclude, path):
     return [file for file in os.listdir(path)
@@ -80,11 +80,11 @@ if 'BOARD' not in os.environ:
     board_folder = None
 else:
     board = os.environ['BOARD']
-    board_folder = 'boards/{}/'.format(board)
+    board_folder = 'boards/{}'.format(board)
 
 # Notebook delivery
 def fill_notebooks():
-    src_nb = GIT_DIR + board_folder + 'notebooks'
+    src_nb = os.path.join(GIT_DIR,board_folder,'notebooks')
     dst_nb_dir = '/home/xilinx/jupyter_notebooks/spyn'
     if os.path.exists(dst_nb_dir):
         shutil.rmtree(dst_nb_dir)
@@ -95,7 +95,7 @@ def fill_notebooks():
 
 # Images delivery
 def fill_images():
-    src_nb = GIT_DIR + board_folder + '/images'
+    src_nb = os.path.join(GIT_DIR, board_folder, 'images')
     dst_nb_dir = '/home/xilinx/jupyter_notebooks/spyn/images'
     if os.path.exists(dst_nb_dir):
         shutil.rmtree(dst_nb_dir)
@@ -106,8 +106,8 @@ def fill_images():
 
 # Overlays delivery
 def fill_overlays():
-    src_nb = GIT_DIR + board_folder + '/overlays'
-    dst_nb_dir = '/home/xilinx/spyn'
+    src_nb = os.path.join(GIT_DIR, board_folder,'overlays')
+    dst_nb_dir = os.path.join(dst_base_dir, 'overlays')
     if os.path.exists(dst_nb_dir):
         shutil.rmtree(dst_nb_dir)
     shutil.copytree(src_nb, dst_nb_dir)
@@ -117,8 +117,8 @@ def fill_overlays():
 
 # Overlays delivery
 def fill_lib():
-    src_nb = GIT_DIR + board_folder + '/lib'
-    dst_nb_dir = '/home/xilinx/spyn'
+    src_nb = os.path.join(GIT_DIR, board_folder,'lib')
+    dst_nb_dir = os.path.join(dst_base_dir, 'lib')
     if os.path.exists(dst_nb_dir):
         shutil.rmtree(dst_nb_dir)
     shutil.copytree(src_nb, dst_nb_dir)
@@ -157,6 +157,4 @@ setup(name='spyn',
       }
       )
 	  
-if board:
-    print('Restarting PL server')
-    subprocess.run(['systemctl', 'restart', 'pl_server'])
+
