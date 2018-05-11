a19d1f14b765e492222df7413acfb9d2ba6da5f3
Kiran Vishal Thanjavur Bhaaskar <vishal94@bu.edu>
2018-05-11 15:28:22 -0700

    Update __init__.py

--- spyn/__init__.py
+++ spyn/__init__.py
@@ -33,4 +33,4 @@ __copyright__ = "Copyright 2018, Xilinx"
 __email__ = "kvt@xilinx.com, npurusho@xilinx.com"
 
 from .lib import *
-from .overlays import EddpOverlay
+from .overlays import SpynOverlay
