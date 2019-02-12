3f52b1b0e44316b7c0ceb8f3fd22caea8236a58f
Naveen Purushotham <pnaveennischal@gmail.com>
2019-02-11 17:34:31 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#   Copyright (c) 2018, Xilinx, Inc.
+#   Copyright (c) 2019, Xilinx, Inc.
 #   All rights reserved.
 # 
 #   Redistribution and use in source and binary forms, with or without 
@@ -27,76 +27,46 @@
 #   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
 #   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, find_packages
 from distutils.dir_util import copy_tree
-import glob
-import re
-import shutil
-import subprocess
-import sys
 import os
-import warnings
-from datetime import datetime
+import shutil
+
+# global variables
+board = os.environ['BOARD']
+repo_board_folder = f'boards/{board}/'
+board_notebooks_dir = os.environ['PYNQ_JUPYTER_NOTEBOOKS']
+hw_data_files = []
 
 
-__author__ = "KV Thanjavur Bhaaskar, Naveen Purushotham"
-__copyright__ = "Copyright 2018, Xilinx"
-__email__ = "kvt@xilinx.com, npurusho@xilinx.com"
+# check whether board is supported
+def check_env():
+    if not os.path.isdir(repo_board_folder):
+        raise ValueError("Board {} is not supported.".format(board))
+    if not os.path.isdir(board_notebooks_dir):
+        raise ValueError("Directory {} does not exist.".format(board_notebooks_dir))
 
 
-GIT_DIR = os.path.dirname(os.path.realpath(__file__))
+# copy overlays to python package
+def copy_overlays():
+    src_ol_dir = os.path.join(repo_board_folder, '')
+    dst_ol_dir = os.path.join('spyn', 'bitstream')
+    copy_tree(src_ol_dir, dst_ol_dir)
+    hw_data_files.extend([os.path.join("..", dst_ol_dir, f) for f in os.listdir(dst_ol_dir)])
 
 
-# Board specific package delivery setup
-def exclude_from_files(exclude, path):
-    return [file for file in os.listdir(path)
-            if os.path.isfile(os.path.join(path, file))
-            and file != exclude]
-
-def find_overlays(path):
-    return [f for f in os.listdir(path)
-            if os.path.isdir(os.path.join(path, f))
-            and len(glob.glob(os.path.join(path, f, "*.bit"))) > 0]
-
-def collect_pynq_overlays():
-    overlay_files = []
-    overlay_dirs = find_overlays(board_folder)
-    for ol in overlay_dirs:
-        copy_tree(os.path.join(board_folder, ol),
-                        os.path.join("spyn/overlays", ol))
-        newdir = os.path.join("spyn/overlays", ol)
-        files = exclude_from_files('makefile', newdir)
-        overlay_files.extend(
-                [os.path.join("..", newdir, f) for f in files])
-    return overlay_files
-
-if 'BOARD' not in os.environ:
-    print("Please set the BOARD environment variable "
-          "to get any BOARD specific overlays (e.g. Pynq-Z1).")
-    board = None
-    board_folder = None
-    data_files = None
-else:
-    board = os.environ['BOARD']
-    board_folder = 'boards/{}'.format(board)
-    data_files = collect_pynq_overlays()
-
-
-# Notebook delivery
-def fill_notebooks():
-    src_nb = os.path.join(GIT_DIR, 'notebooks')
-    dst_nb_dir = '/home/xilinx/jupyter_notebooks/spyn'
+# copy notebooks to jupyter home
+def copy_notebooks():
+    src_nb_dir = os.path.join(f'', 'notebooks')
+    dst_nb_dir = os.path.join(board_notebooks_dir, 'spyn')
     if os.path.exists(dst_nb_dir):
         shutil.rmtree(dst_nb_dir)
-    shutil.copytree(src_nb, dst_nb_dir)
-
-    print("Filling notebooks done ...")
+    copy_tree(src_nb_dir, dst_nb_dir)
 
 
-if len(sys.argv) > 1 and sys.argv[1] == 'install':
-    fill_notebooks()
-
+check_env()
+copy_overlays()
+copy_notebooks()
 
 setup(name='spyn',
       version='1.0',
